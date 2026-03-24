1a08031453c7befa25d615ebd45203d886ba7fdb
UniPM <zoc6x8voc@mozmail.com>
2026-03-23 20:30:56 -0500

    0.3, readded 0.2.1 removed features n added some cool checks :>

--- LCEAuth.cs
+++ LCEAuth.cs
@@ -38,6 +38,7 @@ public class PlayerDB
 	public string? passCrypt { get; set; }
 	public Location? coords { get; set; }
 	public string? ipAddress { get; set; }
+	public DateTime? ipReset { get; set; }
 	public Guid? uid { get; set; }
 }
 
@@ -127,41 +128,62 @@ public class AuthListener : Listener
 		return (getPlr != null && getPlr.GetIP != null && BCrypt.Net.BCrypt.Verify(plr!.getAddress().getAddress().getHostAddress(), getPlr.GetIP)); // so long (thats what she said) - uni
 	}
 
-	public static void tpPlr(Player plr)
-	{
-		var col = Database.Instance.GetCollection<PlayerDB>("playerdb");
-		var getPlr = col.FindOne(LiteDB.Query.EQ("Name", plr.getName()));
-		if (getPlr?.coords != null)
-		{
-			plr.teleport(getPlr.coords);
-		}
-	}
+//	public static void tpPlr(Player plr)
+//	{
+//		var col = Database.Instance.GetCollection<PlayerDB>("playerdb");
+//		var getPlr = col.FindOne(LiteDB.Query.EQ("Name", plr.getName()));
+//		if (getPlr?.coords != null)
+//		{
+//			plr.teleport(getPlr.coords);
+//		}
+//	}
 
 	public static List<string> unauthedUsrs = // god i fucking hate c# sometimes - uni
 		new List<string>();
+
 	public static void AuthInit(Player plr)
 	{
+		// username change code - uni
+		var col1 = Database.Instance.GetCollection<PlayerDB>("playerdb"); // unique name ikr - uni
+                var plrCheckName = col1.Find(LiteDB.Query.EQ("uid", plr.getUniqueId()));
+		
+		foreach (PlayerDB playr in plrCheckName)
+		{
+			if (isReal(plr.getName()))
+			{
+				break; // the last thing you want is a broken db is what i always say :3 - uni
+			}
+			if (playr.Name != plr.getName())
+			{
+				playr.Name = plr.getName(); // set username
+				col1.Update(playr); // i have pre-act tmrw and i wanna die - uni
+				break; // one potential problem i see with this is someone creating 3 accs (somehow), logging into one and then logging into the other. Would that not create two accounts with the same name??? - uni
+				// fixed the problem above using an israel check ^ - uni
+			}
+		}
+
 		if (addressCheck(plr)) {
 			plr.sendMessage($"Logged in as {plr.getName()}");
-			tpPlr(plr);
+			Console.WriteLine($"PlayerDB Location for {plr.getName()}: {getPlrDB(plr.getName()).coords}");
 			return;
 		}
-		var col = Database.Instance.GetCollection<PlayerDB>("playerdb");
-		var coords = col.Find(LiteDB.Query.EQ("Name", plr.getName())).Select(x => new {coordins = x.coords}).FirstOrDefault()?.coordins;
-		if (isReal(plr.getName()) && coords != null)
-		{
-			plr.teleport(new Location(coords.getWorld(), 0, 1000, 0));
-		}
+//		var col = Database.Instance.GetCollection<PlayerDB>("playerdb");
+//		var coords = col.Find(LiteDB.Query.EQ("Name", plr.getName())).Select(x => new {coordins = x.coords}).FirstOrDefault()?.coordins;
+//		if (isReal(plr.getName()) && coords != null)
+//		{
+//			plr.teleport(new Location(coords.getWorld(), 0, 1000, 0));
+//		}
 		unauthedUsrs.Add(plr.getName());
 		plr.sendMessage("LCEAuth");
-        	plr.sendMessage("Type password to continue. /auth <password>"); // [TODO] add 30s wait - uni
+        	plr.sendMessage("Type password to continue. /auth <password>"); // [TODO] add 30s wait - uni (idk when tf i'll ever add this - uni 3/23/26)
 		plr.sendMessage("Optionally, you can input '/auth <password> -noip' to prevent IP tracking.");
-		if (getPlrDB(plr.getName())?.uid == null) { plr.sendMessage("You can also input '/auth <password -nouid>' to prevent UID tracking. You can combine both flags to get '-noip-nouid'."); }
+//		if (getPlrDB(plr.getName())?.uid == null) { plr.sendMessage("You can also input '/auth <password -nouid>' to prevent UID tracking. You can combine both flags to get '-noip-nouid'."); }
 		if (!isReal(plr.getName())) plr.sendMessage("No account found! Register with /areg <password> <confirmpassword>");
 	}
 
 	public static bool AuthFinish(Player plr, string[] args)
 	{
+		if (!(args.Length >= 1)) return false;
 		if (!isReal(plr.getName())) return false;
 		if (testPass(plr.getName(), args[0])) {
 			unauthedUsrs.Remove(plr.getName());
@@ -171,10 +193,15 @@ public class AuthListener : Listener
 				.FirstOrDefault();
 			
 			if (getPlr == null) return false;
-
-			if (!args[1].Contains("-noip")) getPlr.ipAddress = BCrypt.Net.BCrypt.HashPassword(plr!.getAddress().getAddress().getHostAddress()); // why the hell is it like this?? - uni
-			if (!args[1].Contains("-nouid") && getPlr.uid == null) getPlr.uid = plr.getUniqueId(); // so much more simple than ^ this shit - uni
-			tpPlr(plr);
+			if (args.Length == 2) {
+				if (!args[1].Contains("-noip")) getPlr.ipAddress = BCrypt.Net.BCrypt.HashPassword(plr!.getAddress().getAddress().getHostAddress()); // why the hell is it like this?? - uni
+//				if (!args[1].Contains("-nouid") && getPlr.uid == null) getPlr.uid = plr.getUniqueId(); // so much more simple than ^ this shit - uni
+			} else {
+				getPlr.ipAddress = BCrypt.Net.BCrypt.HashPassword(plr!.getAddress().getAddress().getHostAddress()); // why the hell is it like this?? - uni
+//                              getPlr.uid = plr.getUniqueId(); // so much more simple than ^ this shit - uni
+//                              don't ever think about adding this shit again - uni
+			} // copying and pasting is my passion - uni
+//			tpPlr(plr);
 
 			col.Update(getPlr);
 			// hashed for better protection :> - uni
@@ -185,6 +212,8 @@ public class AuthListener : Listener
 
 	public static bool cmdRecover(string[] args)
 	{
+		if (args.Length != 2) return false;
+
 		if (string.IsNullOrEmpty(args[1])) { Console.WriteLine("[AAuth] at /authadmin: missing arg Player"); return false; }
                 if (!AuthListener.isReal(args[1])) { Console.WriteLine($"[AAuth] at /authadmin: Player {args[1]} is not registered!"); return false; }
                 string newPass = AAdmin.passGen(); // gen new pass - uni
@@ -195,17 +224,22 @@ public class AuthListener : Listener
 		if (getPlr == null) { Console.WriteLine($"[AAuth] getPlr failed: null"); return false; }
 
                 getPlr.passCrypt = BCrypt.Net.BCrypt.HashPassword(newPass);
+		
+		getPlr.ipAddress = null;
 
                 col.Update(getPlr);
 
                 if (!AuthListener.testPass(args[1], newPass)) return false;
-
+		
+		FourKit.getPlayer(args[1]).kickPlayer();
                 Console.WriteLine($"[AAuth] Recovered {args[1]}! New password: {newPass}");
                 return true;
 	}
 
 	public static bool cmdChPass(string[] args) // I FUCKING HATE LITEDB!!! - uni
 	{
+		if (args.Length != 3) return false;
+
 		if (string.IsNullOrEmpty(args[1])) { Console.WriteLine("[AAuth] at /authadmin: missing arg Player"); return false; }
                 if (!AuthListener.isReal(args[1])) { Console.WriteLine($"[AAuth] at /authadmin: Player {args[1]} is not registered!"); return false; }
 
@@ -215,11 +249,14 @@ public class AuthListener : Listener
 		if (getPlr == null) { Console.WriteLine($"[AAuth] getPlr failed: null"); return false; }
 
                 getPlr.passCrypt = BCrypt.Net.BCrypt.HashPassword(args[2]);
+		
+		getPlr.ipAddress = null;
 
                 col.Update(getPlr);
 
                 if (!AuthListener.testPass(args[1], args[2])) return false;
-
+		
+		FourKit.getPlayer(args[1]).kickPlayer();
                 Console.WriteLine($"[AAuth] Changed {args[1]}'s password. New password: {args[2]}");
                 return true;
 
@@ -311,20 +348,18 @@ public class AuthListener : Listener
 	public void onLeave(PlayerQuitEvent e)
 	{
 		if (unauthedUsrs.Contains(e.getPlayer().getName())) unauthedUsrs.Remove(e.getPlayer().getName());
-		else {
-			var col = Database.Instance.GetCollection<PlayerDB>("playerdb");
-			var getPlr = col.Find(LiteDB.Query.EQ("Name", e.getPlayer().getName())).FirstOrDefault();
-
-			if (getPlr == null) return;
-
-			var plrCrds = e.getPlayer().getLocation();
-
-			getPlr.coords = plrCrds;
-
-			Console.WriteLine($"{e.getPlayer().getName()} left with internal location: {e.getPlayer().getLocation()}");
-
-			col.Update(getPlr); // i pray to god this works lol - uni
-		}
+//		else {
+//			var col = Database.Instance.GetCollection<PlayerDB>("playerdb");
+//			var getPlr = col.Find(LiteDB.Query.EQ("Name", e.getPlayer().getName())).FirstOrDefault();
+//
+//			if (getPlr == null) return;
+//
+//			getPlr.coords = e.getPlayer().getLocation();
+//
+//			Console.WriteLine($"{e.getPlayer().getName()} left with internal location: {e.getPlayer().getLocation()}");
+//
+//			col.Update(getPlr); // i pray to god this works lol - uni
+//		}
 	}
 }
 public class Auth : CommandExecutor
@@ -348,6 +383,8 @@ public class Areg : CommandExecutor // areg is the account register - uni
 		if (!(sender is Player)) return false; // ik why this is needed now :3 - uni
 		Player p = (Player)sender;
 		
+		if (args.Length != 2) return false;
+
 		if (string.IsNullOrEmpty(args[0])) return false;
 
 		if (args[0] != args[1]) return false; // basic password confirmation - uni
@@ -374,16 +411,18 @@ public class AAdmin : CommandExecutor
 	public bool onCommand(CommandSender sender, Command command, string label, string[] args)
 	{
 		if (!(sender is ConsoleCommandSender)) return false; // this is server line ONLY! - uni
+		
+		if (!(args.Length >= 1)) return false;
 
 		if (string.IsNullOrEmpty(args[0])) return false;
 
 		if (args[0] == "recover")
 		{
-			AuthListener.cmdRecover(args);
+			return AuthListener.cmdRecover(args);
 		}
 		if (args[0] == "changepass")
 		{
-			AuthListener.cmdChPass(args);
+			return AuthListener.cmdChPass(args);
 		}
 		return false;
 	}
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # building script bc building is exhausting
 
 dotnet build LCEAuth.csproj
-mv ./bin/Debug/net10.0/LCEAuth.dll ../plugins/LCEAuth.dll
+mv ./bin/Debug/net10.0/LCEAuth.dll ../plugins/LCEAuth/LCEAuth.dll
 cd ../
